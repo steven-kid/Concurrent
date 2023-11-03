bfd55b1a58f1fad0266dae2561bff1c4c093224f
steven-kid <stevenkid404@gmail.com>
2023-11-03 22:46:01 +1030

    first commit

---
+++ src/BookingResult.java
@@ -0,0 +1,16 @@
+public class BookingResult {
+
+	public int jobID;
+	public Passenger passenger;
+	public Driver driver;
+	public long tripDuration;
+	
+	public BookingResult(int jobID, Passenger passenger, Driver driver, long tripDuration)
+	{
+		this.jobID = jobID;
+		this.passenger = passenger;
+		this.driver = driver;
+		this.tripDuration = tripDuration;
+	}
+	
+}
---
+++ src/Passenger.java
@@ -0,0 +1,13 @@
+public class Passenger extends Person
+{
+	
+	public Passenger(String name, int maxSleep) {
+		super(name, maxSleep);
+	}
+
+	public int getTravelTime()
+	{
+		return (int)(Math.random() * maxSleep);
+	}
+
+}
